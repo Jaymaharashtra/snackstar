c31af960704f04bca9b4c145953ae5269e1d3572
prathamesh <prathameshap2000@gmail.com>
2024-01-01 01:55:52 +0530

    Init

---
+++ frontend/src/app/shared/Models/Food.ts
@@ -0,0 +1,11 @@
+export class data {
+    id!:number;
+    name!:String;
+    imageUrl!:String;
+    Price!:number;
+    tag?:String[];
+    origins!:String[];
+    cooktime!:String;
+    favorite!:boolean;
+    
+}
